df4704d1324414c5ad98fe113a3480df8d777ef7
Jorge Venegas <brangelina01@gmail.com>
2020-07-17 06:37:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,9 @@
 def line(katz_deli)
   if katz_deli.count > 1
    output = "The line is currently:"
-   katz_deli.each_with_index do |val, index| output << " #{index+1}. #{val}"
+   katz_deli.each_with_index{|val, index| output << " #{index+1}. #{val}"
    puts output
    else 
      puts "The line is currently empty."
  end
  end 
-end
