6b491bcf11ff326e6076aa261a37125ac0072931
Jorge Venegas <brangelina01@gmail.com>
2020-07-17 06:22:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,19 +1,8 @@
-def line (katz_deli) 
-  if 
-    katz_deli.length == 0 
-     puts "The line is currently empty."
-  else
-    katz_deli.each do |index, name|
-      counter = 0 
-     puts "The line is currently: #{index}. #{name}"
-      counter += 1
-  end 
-  end 
-end
-def line(array)
-  if array.count > 1
+
+def line(katz_deli)
+  if katz_deli.count > 1
    puts = "The line is currently:"
-   array.each_with_index {|val, index| output << " #{index+1}. #{val}"}
+   katz_deli.each_with_index {|val, index| output << " #{index+1}. #{val}"}
    puts output
    else 
      puts "The line is currently empty."
