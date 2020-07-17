a5d9de8ec18104508b879e304fc5b88c903d4239
Jorge Venegas <brangelina01@gmail.com>
2020-07-17 06:28:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(katz_deli)
     puts "The line is currently empty."
   else 
     katz_deli.each_with_index do |name, index|
-    puts 
+    puts "The line is currently: 
 
 def line(katz_deli)
   if katz_deli.count > 1
